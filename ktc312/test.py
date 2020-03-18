43e43bb202fab14e0a1bdda4ca236d82a3184823
swedge <scott.wedge@gmail.com>
2020-03-17 20:06:45 -0400

    Fixes typo 'asertFalse'

--- ktc312/test.py
+++ ktc312/test.py
@@ -7,7 +7,7 @@ class MyTestCase(unittest.TestCase):
     def raw_input(self):
         response = range(1800, 2013, 1)
         self.assertTrue(1999 in response)
-        self.asertFalse(0000 in response)
+        self.assertFalse(0000 in response)
 
 
 if __name__ == '__main__':
